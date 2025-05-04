13614ef20cf563d54f76f60c4dd6f48fe1e76cc1
ivanMareev <rain12003@yandex.ru>
2025-05-04 12:31:04 +1100

    docker

--- expressionism-server/config.py
+++ expressionism-server/config.py
@@ -1,41 +1,25 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
-from flask_migrate import Migrate
-
-
+import os
 
 app = Flask(__name__)
 
 # Настройки приложения
-app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{app.root_path}/expressionism.sqlite"
-app.config['JWT_SECRET_KEY'] = 'b65d1f49e7a4f79d6f4f758d75d06445'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DATABASE_URL"]
 app.config['SECRET_KEY'] = 'your_secret_key'
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = 120  # 1 час
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = 120
+app.config["JWT_REFRESH_TOKEN_EXPIRES"] = 86400
 
-
-
-# TODO вввести в эксплуатацию рефреш токен 
-app.config["JWT_REFRESH_TOKEN_EXPIRES"] = 86400  # 1 день
-app.json.sort_keys = False
-app.debug = True
-
-
-
-# CORS и JWT
+db = SQLAlchemy(app)
 CORS(app)
 jwt = JWTManager(app)
 
-# База данных
-class Base(DeclarativeBase):
-    pass
-
-db = SQLAlchemy(app, model_class=Base)
+with app.app_context():
+    res = db.create_all()
+    print("Таблицы созданы.", res)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
 
+import main
--- expressionism-server/main.py
+++ expressionism-server/main.py
@@ -1,39 +1,26 @@
+from flask_migrate import Migrate
+from flask.cli import FlaskGroup
 from config import app, db
-from admin import init_admin
-from misc import fill_sample_data
-from auth_api.auth_api import auth_api_blueprint
-from expressionism.api import expressionism_api_blueprint, remove_old_pdfs
 from html_generator.generator_fisrt_level import generate_first_level1_api_blueprint
 from html_generator.generator_second_level import generate_second_level_api_blueprint
 from html_generator.generator_third_level import generate_third_level_api_blueprint
-from sqlalchemy import text
+from admin import init_admin
 
-app.register_blueprint(expressionism_api_blueprint, url_prefix="/")
-app.register_blueprint(auth_api_blueprint, url_prefix="/api/v1")
+# Регистрация роутов и админки
 app.register_blueprint(generate_first_level1_api_blueprint, url_prefix="/api/v1")
 app.register_blueprint(generate_second_level_api_blueprint, url_prefix="/api/v1")
 app.register_blueprint(generate_third_level_api_blueprint, url_prefix="/api/v1")
+init_admin()
 
+# Настройка миграций
+migrate = Migrate(app, db)
 
+# CLI-команда для flask
+cli = FlaskGroup(app)
+
+@app.route("/")
+def index():
+    return "Server is running!"
 
 if __name__ == "__main__":
-    with app.app_context():
-        init_admin()
-        # db.drop_all()
-        # db.create_all()
-        # db.session.execute(text("ALTER TABLE layout_variant_3 ADD COLUMN is_active BOOLEAN;"))
-        # db.session.commit()
-
-        # Выполнение второго запроса для добавления lvl3_id
-        # db.session.execute(text("ALTER TABLE template_lvl3 ADD COLUMN lvl3_id INTEGER;"))
-        # db.session.commit()
-
-        # # Выполнение третьего запроса для добавления always_eat
-        # db.session.execute(text("ALTER TABLE template_lvl3 ADD COLUMN always_eat BOOLEAN;"))
-        # db.session.commit()
-
-        
-        # fill_sample_data(db)
-        remove_old_pdfs()
-
-    app.run(debug=True, use_debugger=True, use_reloader=False, passthrough_errors=True)
+    cli()
